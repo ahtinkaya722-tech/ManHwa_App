5b8aee653cbe7e2795c989f8bb2cc8c5b8fdad0a
A Htin Ka Ya <ahitnkaya722@gmail.com>
2026-05-28 13:38:41 +0630

    update vite config

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ return (
         <Routes>
        
             <Route path="/" element={<Home/>}/>
+            <Route path="*" element={<Home/>}/>
                {/* <Route path="/trending" element={} />
              <Route path="/latest" element={} />
              <Route path="/library" element={} /> */}
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- <BrowserRouter>
+ <BrowserRouter basename={import.meta.env.BASE_URL}>
     <App />
   </BrowserRouter>
 )
